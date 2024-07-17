7eb646ec0c5e3cafa4d1b7170f14d8fb26d5b762
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 13:58:10 +0200

    feat: init signup page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
   
 const router = createBrowserRouter([
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       path: "/",
       element: <Home />,
     },
+    {
+      path: "/signup",
+      element: <Signup />
+    }
 ]);
 
 export default router;
