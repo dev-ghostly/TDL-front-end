013e839f321824d000eb6a0b0eeae3713b48164b
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 13:28:02 +0200

    feat: dd colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    // add colors
+    colors : {
+      one : "#F0F0F0",
+      two : "#E3EAE9",
+      three: "#BEE5D3",
+      four : "#C0D6D2",
+      five : "#4DB97E",
+      six : "#C56363"
+    },
     extend: {},
   },
   plugins: [],
