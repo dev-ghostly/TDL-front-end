3ecc03f746748589cd8e6a97abf35b29f40a7341
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 14:36:36 +0200

    feat: handle change input login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,16 @@ import { useState } from 'react';
 import axios from 'axios';
 
 export default function Login(){
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            [e.target.id]: e.target.value
+        });
+    }
     return <>
         <Navbar />
         <main className="flex max-w-6xl mx-auto font-website">
@@ -13,11 +23,11 @@ export default function Login(){
                 <form className="mt-52 bg-two p-4 rounded-lg">
                     <div className="flex flex-col mb-4">
                         <label htmlFor="username" className="text-sm mb-2">Username</label>
-                        <input type="text" id="username" className="p-2 bg-four rounded-full" />
+                        <input type="text" id="username" className="p-2 bg-four rounded-full" onChange={(e) => handleChange(e)} />
                     </div>
                     <div className="flex flex-col mb-4">
                         <label htmlFor="password" className="text-sm mb-2">Password</label>
-                        <input type="password" id="password" className="p-2 bg-four rounded-full" />
+                        <input type="password" id="password" className="p-2 bg-four rounded-full" onChange={(e) => handleChange(e)} />
                     </div>
                     <div className='flex justify-center'>
                         <button className="bg-four text-white p-2 rounded-full px-8">Login</button>
