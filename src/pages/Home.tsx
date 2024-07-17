371d7ab04795d09aea1a87e8791f944bd1fd35bc
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 13:54:06 +0200

    feat: home page structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import Navbar from "../components/Navbar";
+
 export default function Home(){
-    return <div>Hello World !</div>
+    return <div>
+        <Navbar/>
+        <main className="flex max-w-6xl mx-auto font-website">
+            <div className="w-1/2"></div>
+            <div className="w-1/2 flex flex-col items-center">
+                <h2 className=" text-4xl font-bold tracking-wide mt-60">stay organized,</h2>
+                <h2 className=" text-4xl font-bold tracking-wide mt-3">stay relaxed</h2>
+                <button className="mt-8 bg-three font-semibold uppercase px-8 py-2 rounded-full">Get started</button>
+            </div>
+        </main>
+    </div>
 }
