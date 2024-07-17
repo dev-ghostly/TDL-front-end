0f46a4de02c7a41f02846a96acbec445206aefde
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 17:09:05 +0200

    feat: main structure of the app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,3 +1,23 @@
+import Logo from "../components/Logo";
+import { useDispatch, useSelector } from "react-redux";
+
 export default function App(){
-    return <main className="max-w-6xl mx-auto">App</main>
+    const dispatch = useDispatch();
+    
+    return <main className="max-w-6xl mx-auto flex font-website">
+        <div className="w-1/4 h-screen bg-two p-4 flex flex-col justify-between">
+            <Logo />
+            <div>
+                <ul className="list-disc pl-4">
+                    <li>Settings</li>
+                    <li>Log out</li>
+                </ul>
+            </div>
+        </div>
+        <div className="w-3/4 h-screen">
+            <div className="mt-4">
+                <h1 className="font-semibold ml-8 text-2xl">Your Lists</h1>
+            </div>
+        </div>
+    </main>
 }
