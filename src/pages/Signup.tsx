686c66ec2c75947c41664ae0f456047ce75f3f5b
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 14:12:53 +0200

    feat: handleChange of form signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,18 @@
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 export default function Signup(){
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            [e.target.id]: e.target.value
+        });
+    }
     return <>
         <Navbar />
         <main className="flex max-w-6xl mx-auto font-website">
@@ -11,15 +23,15 @@ export default function Signup(){
                 <form className="mt-52 bg-two p-4 rounded-lg">
                     <div className="flex flex-col mb-4">
                         <label htmlFor="username" className="text-sm mb-2">Username</label>
-                        <input type="text" id="username" className="p-2 bg-four rounded-full" />
+                        <input type="text" id="username" className="p-2 bg-four rounded-full" onChange={(e) => handleChange(e)} />
                     </div>
                     <div className="flex flex-col mb-4">
                         <label htmlFor="password" className="text-sm mb-2">Password</label>
-                        <input type="password" id="password" className="p-2 bg-four rounded-full" />
+                        <input type="password" id="password" className="p-2 bg-four rounded-full" onChange={(e) => handleChange(e)} />
                     </div>
                     <div className="flex flex-col mb-4">
                         <label htmlFor="confirm-password" className="text-sm mb-2">Confirm Password</label>
-                        <input type="password" id="confirm-password" className="p-2 bg-four rounded-full" />
+                        <input type="password" id="confirm-password" className="p-2 bg-four rounded-full" onChange={(e) => handleChange(e)} />
                     </div>
                     <div className="flex justify-center mt-10">
                         <button className="bg-black text-one py-2 px-8 bg-five rounded-full">Sign Up</button>
