4097ffa1713e3e51d92a3527b87b97ea25043aeb
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 14:35:49 +0200

    feat: form login done

---
+++ src/pages/Login.tsx
@@ -0,0 +1,29 @@
+import Navbar from '../components/Navbar';
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Login(){
+    return <>
+        <Navbar />
+        <main className="flex max-w-6xl mx-auto font-website">
+            <div className="w-1/2 flex justify-center">
+                <h1 className="text-4xl font-semibold mt-80">login.</h1>
+            </div>
+            <div className="w-1/2">
+                <form className="mt-52 bg-two p-4 rounded-lg">
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="username" className="text-sm mb-2">Username</label>
+                        <input type="text" id="username" className="p-2 bg-four rounded-full" />
+                    </div>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="password" className="text-sm mb-2">Password</label>
+                        <input type="password" id="password" className="p-2 bg-four rounded-full" />
+                    </div>
+                    <div className='flex justify-center'>
+                        <button className="bg-four text-white p-2 rounded-full px-8">Login</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    </>
+}
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
   
 const router = createBrowserRouter([
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
     {
       path: "/signup",
       element: <Signup />
+    },
+    {
+      path: "/login",
+      element: <Login />
     }
 ]);
 
