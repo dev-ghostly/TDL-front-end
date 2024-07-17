098e073c8842bd3f9199be7eb6c7b9e360d47738
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-17 13:23:50 +0200

    feat: init of react router dome

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {store} from './redux/store'
 import { Provider } from 'react-redux'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <h1>Home</h1>
+    <RouterProvider router={router} />
   </Provider>,
 )
