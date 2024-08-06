d67778fbd0d169b67d877c9e2b745d00c73e1639
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-08-06 20:23:43 +0200

    feat: add image on main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
-
+import picture from '../assets/43268.jpg'
 export default function Home(){
     return <div>
         <Navbar/>
         <main className="flex max-w-6xl mx-auto font-website">
-            <div className="w-1/2"></div>
+            <div className="w-1/2">
+                <img src={picture} alt="home" className="w-full h-full object-cover"/>
+            </div>
             <div className="w-1/2 flex flex-col items-center">
                 <h2 className=" text-4xl font-bold tracking-wide mt-60">stay organized,</h2>
                 <h2 className=" text-4xl font-bold tracking-wide mt-3">stay relaxed</h2>
